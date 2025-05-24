1be3e98563ca41bda9f154e23f72d80a0dd264a7
sim1gariglio <simone.gariglio1@gmail.com>
2025-05-24 18:11:30 +0200

    parseinput.c

--- parseInput.c
+++ parseInput.c
@@ -1,71 +1,75 @@
 #include "hotrace.h"
 
-void parse_input(t_map **entries, int *size, int capacity, int count)
+static t_map	*expand_entries(t_map *entries, int *capacity, int count)
 {
-    // Alloca l'array iniziale
-    *entries = malloc(capacity * sizeof(t_map));
-    if (!*entries)
-        return;
-    
-    // Leggi e rialloca all'occorrenza
-    while (1) {
-        char *keyword = fast_read_line(0, 0);
-        if (!keyword || keyword[0] == '\0') {
-            free(keyword);
-            break;
-        }
-        
-        char *value = fast_read_line(0, 0);
-        if (!value) {
-            free(keyword);
-            break;
-        }
-        
-        // Espandi l'array se necessario
-        if (count >= capacity) {
-            capacity *= 2;
-            t_map *new_entries = malloc(capacity * sizeof(t_map));
-            if (!new_entries) {
-                // Gestisci l'errore
-                free(keyword);
-                free(value);
-                
-                for (int i = 0; i < count; i++) {
-                    free((*entries)[i].key);
-                    free((*entries)[i].value);
-                }
-                free(*entries);
-                return;
-            }
-            
-            // Copia i dati esistenti
-            for (int i = 0; i < count; i++) {
-                new_entries[i].key = (*entries)[i].key;
-                new_entries[i].value = (*entries)[i].value;
-            }
-            
-            free(*entries);
-            *entries = new_entries;
-        }
-        
-        // Aggiungi la nuova entry
-        (*entries)[count].key = keyword;
-        (*entries)[count].value = value;
-        count++;
-    }
-    
-    // Ridimensiona alla dimensione esatta se necessario
-    if (count < capacity) {
-        t_map *final_entries = malloc(count * sizeof(t_map));
-        if (final_entries) {
-            for (int i = 0; i < count; i++) {
-                final_entries[i].key = (*entries)[i].key;
-                final_entries[i].value = (*entries)[i].value;
-            }
-            free(*entries);
-            *entries = final_entries;
-        }
-    }
-    
-    *size = count;
+	int		i;
+	int		new_capacity;
+	t_map	*new_entries;
+
+	i = 0;
+	new_capacity = (*capacity) * 2;
+	new_entries = malloc(new_capacity * sizeof(t_map));
+	if (!new_entries)
+		return (NULL);
+	while (i < count)
+	{
+		new_entries[i].key = entries[i].key;
+		new_entries[i].value = entries[i].value;
+		i++;
+	}
+	free(entries);
+	*capacity = new_capacity;
+	return (new_entries);
+}
+
+static t_map	*shrink_entries(t_map *entries, int count)
+{
+	int		i;
+	t_map	*final_entries;
+
+	i = 0;
+	final_entries = malloc(count * sizeof(t_map));
+	if (!final_entries)
+		return (entries);
+	while (i < count)
+	{
+		final_entries[i].key = entries[i].key;
+		final_entries[i].value = entries[i].value;
+		i++;
+	}
+	free(entries);
+	return (final_entries);
+}
+
+void	parse_input(t_map **entries, int *size, int capacity, int count)
+{
+	char	*key;
+	char	*val;
+
+	*entries = malloc(capacity * sizeof(t_map));
+	if (!*entries)
+		return ;
+	while (1)
+	{
+		key = fast_read_line(0, 0);
+		if (!key || key[0] == '\0')
+		{
+			free(key);
+			break ;
+		}
+		val = fast_read_line(0, 0);
+		if (!val)
+		{
+			free(key);
+			break ;
+		}
+		if (count >= capacity)
+			*entries = expand_entries(*entries, &capacity, count);
+		(*entries)[count].key = key;
+		(*entries)[count].value = val;
+		count++;
+	}
+	if (count < capacity)
+		*entries = shrink_entries(*entries, count);
+	*size = count;
 }
